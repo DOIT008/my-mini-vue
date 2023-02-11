57499100c3c9337fdcee7567d143514bc6c7461a
DOIT <zsf900730@163.com>
2023-02-12 07:44:11 +0800

    实现scheduler

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,12 +1,13 @@
 class ReactiveEffect { 
-  private _fn:Function
-  constructor(fn:Function) { 
+  private _fn: Function
+  // 为啥加public，为了在实例上能拿到该参数
+  constructor(fn:Function,public scheduler?) { 
     this._fn = fn
   }
   run() { 
     // 当执行run的时候，这里的this就是ReactiveEffect实例，说白了就是下面的_effect
     activeEffect = this;
-    this._fn()
+    return this._fn()
   }
 }
 
@@ -40,16 +41,24 @@ export function track(target:any, key:any) {
 // 声明一个变量，以便拿到 _effect
 let activeEffect:any;
 // 副作用,当effect执行的时候，其内部的函数fn也会执行
-export function effect(fn: Function) {
+export function effect(fn: Function, option:any = {}) {
+  const scheduler = option.scheduler
   // 被收集的对象,为啥是它呢，是因为它上面有run方法，可以修改数据？
-  const _effect = new ReactiveEffect(fn);
+  const _effect = new ReactiveEffect(fn,scheduler);
+  // 调用这个的时候已经返回foo
   _effect.run();
+  // 其实返回的就是run方法，为啥要这么写，是为了处理run内部的this指向
+  return _effect.run.bind(_effect)
 }
 
 export function trigger(target:any,key:any) {
   let depsMap = targetMap.get(target);
   let dep = depsMap.get(key);
   for (const effect of dep) {
-    effect.run()
+    if (effect.scheduler) {
+      effect.scheduler()
+    } else { 
+      effect.run()
+    }
   }
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -16,11 +16,57 @@ describe("effect", () => {
       newSum = user.sum + 1000;
     });
     expect(newAge).toBe(11);
-    // expect(newSum).toBe(11000);
+    expect(newSum).toBe(11000);
     // 更新数据,触发set
     user.age++;
     expect(newAge).toBe(12);
-    // user.sum++;
-    // expect(newSum).toBe(11001);
+    user.sum++;
+    expect(newSum).toBe(11001);
+  })
+  
+  it('当调用effect时返回一个runner', () => { 
+  // effect(fn)——>返回一个函数runner——>调用runner——>执行fn——>
+    let foo = 19;
+    const runner = effect(() => { 
+      foo++;
+      return 'foo'
+    });
+    expect(foo).toBe(20);
+    // 重新调用了run方法
+    const r = runner();
+    expect(foo).toBe(21);
+    expect(r).toBe('foo');
+  })
+
+  it('scheduler', () => { 
+    /**
+     * 1. 通过 effect 的第二个参数给定一个scheduler函数
+     * 2. effect第一次执行的时候会执行fn
+     * 3. 当响应式对象set不会执行fn，而是执行scheduler
+     * 4. 当执行runner的时候会再次执行fn
+     *  */
+
+    let dummy;
+    let run: any;
+    const scheduler = vitest.fn(() => {
+      run = runner
+    })
+    const obj = reactive({ foo: 1 });
+    const runner = effect(() => { 
+      dummy = obj.foo;
+    }, {
+      scheduler
+    })
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1);
+    // 更新操作，should be called on first trigger
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1);
+    // should not run yet
+    expect(dummy).toBe(1)
+    // manually run
+    run();
+    // should have run
+    expect(dummy).toBe(2)
   })
 })
