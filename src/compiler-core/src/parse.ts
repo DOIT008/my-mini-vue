85a6857ecc3b3c9fdc66842b4a5b244e333591b6
DOIT <zsf900730@163.com>
2023-02-25 07:30:24 +0800

    解析 element 标签

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,52 +1,87 @@
 import { NodeTypes } from "./ast";
+
+enum TagType {
+  start,
+  end
+} 
 export function baseParse(content: string) {
-  const context = createParserContext(content)
-  return createRoot(parserChildren(context))
+  const context = createParserContext(content);
+  return createRoot(parserChildren(context));
 }
 
-function parserChildren(context) { 
+function parserChildren(context) {
   const nodes: any[] = [];
   let node;
-  if (context.source.startsWith('{{')) {
+  let s = context.source;
+  if (s.startsWith("{{")) {
     node = parseInterpolation(context);
+  } else if (s[0] === "<") {
+    // 处理element <div></div>
+    if (/[a-z]/i.test(s[1])) {
+     node = parseElement(context);
+    }
   }
   nodes.push(node);
-  return nodes
+  return nodes;
 }
 
-function parseInterpolation(context) { 
+function parseElement(context: any) {
+  // 处理前一个标签<div>
+  const element = parseTag(context, TagType.start);
+  // 处理后一个标签 </div>
+  parseTag(context,TagType.end);
+  // 解析出 tag
+  // 删除处理完成的代码
+  return element
+}
+
+function parseTag(context: any,type) {
+  const match = /^<\/?([a-z]*)/i.exec(context.source) as RegExpExecArray;
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+  if(type===TagType.end) return
+  return {
+    type: NodeTypes.ELEMENT,
+    tag: tag,
+  };
+}
+
+function parseInterpolation(context) {
   const openDelimiter = "{{";
   const closeDelimiter = "}}";
   // 获取右侧}}索引值
-  const closeIndex = context.source.indexOf(closeDelimiter,openDelimiter.length);
-  
+  const closeIndex = context.source.indexOf(
+    closeDelimiter,
+    openDelimiter.length
+  );
+
   advanceBy(context, openDelimiter.length);
   const rawContentLength = closeIndex - openDelimiter.length;
   const rawContent = context.source.slice(0, rawContentLength);
   const content = rawContent.trim();
   advanceBy(context, rawContentLength + closeDelimiter.length);
   return {
-    type:NodeTypes.INTERPOLATION,
+    type: NodeTypes.INTERPOLATION,
     content: {
-      type:NodeTypes.SIMPLE_EXPRESSION,
+      type: NodeTypes.SIMPLE_EXPRESSION,
       content: content,
     },
-  }
+  };
 }
- 
-function advanceBy(context: any, length:number) {
+
+function advanceBy(context: any, length: number) {
   context.source = context.source.slice(length);
 }
 
-function createRoot(children) { 
+function createRoot(children) {
   return {
-    children
-  }
+    children,
+  };
 }
 // 创建上下文对象
-function createParserContext(content: string):any {
+function createParserContext(content: string): any {
   return {
-    source:content
-  }
+    source: content,
+  };
 }
-
